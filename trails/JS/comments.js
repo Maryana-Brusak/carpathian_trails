59869b02dcd5b8ad2e6e45d2120568275a3c429c
Maryana Brusak <matkivska.m@gmail.com>
2016-09-02 13:05:20 +0300

    Sumlified comments function (step one)

--- trails/JS/comments.js
+++ trails/JS/comments.js
@@ -6,7 +6,7 @@
 			 	 xhttp.onreadystatechange = function() {
 			    	if (xhttp.readyState == 4 && xhttp.status == 201) {
 			      		commentRender(formData.get('name'), formData.get('comment'));
-			      		resetFunction();
+			      		document.querySelector("form").reset();
 			    	}
 			  	};
 
@@ -18,9 +18,9 @@
 	  		return false;
 		}
 
-		function resetFunction() {
+		/*function resetFunction() {
 				document.querySelector("form").reset();
-		}
+		}*/
 
 		function commentRender(name, comment) {
 			var divName = document.createElement('div');
@@ -36,42 +36,25 @@
 			articleComment.appendChild(divName);
 			articleComment.appendChild(divComment);
 
-/*			var commentReply = document.createElement('div');
-			commentReply.className = ('commentReply');
-			commentReply.innerHTML = 'Відповісти';*/
-
 			var theFirstChild = document.getElementsByClassName('comments')[0].firstChild;
-
 			var sectionComments = document.getElementsByClassName('comments')[0].insertBefore(articleComment, theFirstChild);
-/*			var sectionComments = document.getElementsByClassName('comments')[0].insertBefore(commentReply, theFirstChild);
-*/  		}
+ 		}
 
-  		function getComments(pageId) {
-  			try { 	
-			  	var xhttp;
-			 	 xhttp = new XMLHttpRequest();
-			 	 xhttp.onreadystatechange = function() {
-			    	if (xhttp.readyState == 4 && xhttp.status == 200) {
-			      		console.log(xhttp);
-			      		var comments = JSON.parse(xhttp.responseText);
-			      		console.log(comments);
-			      		myFunction(comments);
-			    	}
-			    	
-			  	};
+  		function getComments(pageId) {				
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (xhttp.readyState == 4 && xhttp.status == 200) {
+					var comments = JSON.parse(xhttp.responseText);				
 
-		 	 	xhttp.open("GET", '/comments/' + pageId, true);
-		  		xhttp.send();
-
-		  		function myFunction(comments) {
-		  			var nameComment;
-		  			var textComment;
 		  			for(var i = 0; i < comments.length; i++) {
-		  				commentRender(comments[i].name, comments[i].comment);	
-		  			}
-		  		}
-	  		}
-	  		catch(e) {}
+	  					commentRender(comments[i].name, comments[i].comment);	
+	  				}
+				}
+
+			};
+
+	 	 	xhttp.open("GET", '/comments/' + pageId, true);
+	  		xhttp.send();
 
 	  		return false;
 		}
