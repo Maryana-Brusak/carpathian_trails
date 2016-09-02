7cd954b04de2857b08bc2ce8ede863faf8e73bc5
Maryana Brusak <matkivska.m@gmail.com>
2016-09-02 13:02:49 +0300

    Filtering function simplified. Checkboxes fixed(click on text part made)

--- trails/JS/script.js
+++ trails/JS/script.js
@@ -3,87 +3,40 @@ var checkBoxes = document.querySelectorAll("#filterSection li input");
   
 for (var i = 0; i < checkBoxes.length; i++) {
     checkBoxes[i].addEventListener("click", filterTrails, false);
-    checkBoxes[i].checked = true;
 }
 
 for (var i = 0; i < trailsToFilter.length; i++) {
 	trailsToFilter[i].filtering = [];
 }
-  
+
+/**
+ * Filter checkbox onclick handler
+ */
 function filterTrails(e) {
-    var clickedTrail = e.target;
-	var dataFiltering = clickedTrail.getAttribute("data-filtering");
+    var clickedFilter = e.target;
+	var filterType = clickedFilter.getAttribute("data-filtering");
 
-      
-    if (clickedTrail.checked == true) {
-       showTrails(clickedTrail.value, "hideItem", "showItem", dataFiltering);
-    } else if (clickedTrail.checked == false) {
-        hideTrails(clickedTrail.value, "showItem", "hideItem", dataFiltering);
-    } 
-}
-
-function hideTrails(itemType, classToRemove, classToAdd, dataFiltering) {
-	for (var i = 0; i < trailsToFilter.length; i++) {
+    for (var i = 0; i < trailsToFilter.length; i++) {
         var currentTrail = trailsToFilter[i];
+		var filterBy = clickedFilter.value;
 
-		if (currentTrail.getAttribute("data-" + dataFiltering) == itemType) {
-			currentTrail.filtering.push(itemType);
+		if (currentTrail.getAttribute("data-" + filterType) == filterBy) {
+			if (clickedFilter.checked == true) {
+				//show hidden trail
+				var index = currentTrail.filtering.indexOf(filterBy);
+				currentTrail.filtering.splice(index, 1);
 
-            removeClass(currentTrail, classToRemove);
-            addClass(currentTrail, classToAdd);
-        } 
-    }
-}
+				if (!currentTrail.filtering.length) {
+					currentTrail.classList.remove("hideItem");
+					currentTrail.classList.add("showItem");
+				}
+			} else {
+				// hide trail
+				currentTrail.filtering.push(filterBy);
+				currentTrail.classList.remove("showItem");
+				currentTrail.classList.add("hideItem");
 
-
-
-function showTrails(itemType, classToRemove, classToAdd, dataFiltering) {
-	for (var i = 0; i < trailsToFilter.length; i++) {
-        var currentTrail = trailsToFilter[i];
-
-		if (currentTrail.getAttribute("data-" + dataFiltering) == itemType) {
-			var index = currentTrail.filtering.indexOf(itemType);
-			currentTrail.filtering.splice(index, 1);
-
-			if (currentTrail.filtering.length == 0) {
-				removeClass(currentTrail, classToRemove);
-                addClass(currentTrail, classToAdd);
-
-			}            
-        } 
-    }
-}
-
-
- 
- function addClass(element, classToAdd) {
-    var currentClassValue = element.className;
-        
-    if (currentClassValue.indexOf(classToAdd) == -1) {
-        if ((currentClassValue == null) || (currentClassValue === "")) {
-            element.className = classToAdd;
-        } else {
-            element.className += " " + classToAdd;
+			}	
         }
-    }
-}
-        
-function removeClass(element, classToRemove) {
-    var currentClassValue = element.className;
-  
-    if (currentClassValue == classToRemove) {
-        element.className = "";
-        return;
-    }
-  
-    var classValues = currentClassValue.split(" ");
-    var filteredList = [];
-  
-    for (var i = 0 ; i < classValues.length; i++) {
-        if (classToRemove != classValues[i]) {
-            filteredList.push(classValues[i]);
-        }
-    }
-  
-    element.className = filteredList.join(" ");
+	}
 }
