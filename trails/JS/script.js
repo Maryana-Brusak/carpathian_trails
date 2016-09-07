f590de6087c2ca22018ff4bffde3e20997aa3028
Maryana Brusak <matkivska.m@gmail.com>
2016-09-07 14:40:30 +0300

    added function loadFilters () for loading filters at simple order page

--- trails/JS/script.js
+++ trails/JS/script.js
@@ -1,42 +1,47 @@
-var trailsToFilter = document.querySelectorAll('#itemsToFilter li');
-var checkBoxes = document.querySelectorAll("#filterSection li input");
-  
-for (var i = 0; i < checkBoxes.length; i++) {
-    checkBoxes[i].addEventListener("click", filterTrails, false);
-}
+function loadFilters () {
 
-for (var i = 0; i < trailsToFilter.length; i++) {
-	trailsToFilter[i].filtering = [];
-}
 
-/**
- * Filter checkbox onclick handler
- */
-function filterTrails(e) {
-    var clickedFilter = e.target;
-	var filterType = clickedFilter.getAttribute("data-filtering");
-
-    for (var i = 0; i < trailsToFilter.length; i++) {
-        var currentTrail = trailsToFilter[i];
-		var filterBy = clickedFilter.value;
-
-		if (currentTrail.getAttribute("data-" + filterType) == filterBy) {
-			if (clickedFilter.checked == true) {
-				//show hidden trail
-				var index = currentTrail.filtering.indexOf(filterBy);
-				currentTrail.filtering.splice(index, 1);
-
-				if (!currentTrail.filtering.length) {
-					currentTrail.classList.remove("hideItem");
-					currentTrail.classList.add("showItem");
-				}
-			} else {
-				// hide trail
-				currentTrail.filtering.push(filterBy);
-				currentTrail.classList.remove("showItem");
-				currentTrail.classList.add("hideItem");
-
-			}	
-        }
+	var trailsToFilter = document.querySelectorAll('#itemsToFilter li');
+	var checkBoxes = document.querySelectorAll("#filterSection li input");
+	  
+	for (var i = 0; i < checkBoxes.length; i++) {
+	    checkBoxes[i].addEventListener("click", filterTrails, false);
 	}
+
+	for (var i = 0; i < trailsToFilter.length; i++) {
+		trailsToFilter[i].filtering = [];
+	}
+
+	/**
+	 * Filter checkbox onclick handler
+	 */
+	function filterTrails(e) {
+	    var clickedFilter = e.target;
+		var filterType = clickedFilter.getAttribute("data-filtering");
+
+	    for (var i = 0; i < trailsToFilter.length; i++) {
+	        var currentTrail = trailsToFilter[i];
+			var filterBy = clickedFilter.value;
+
+			if (currentTrail.getAttribute("data-" + filterType) == filterBy) {
+				if (clickedFilter.checked == true) {
+					//show hidden trail
+					var index = currentTrail.filtering.indexOf(filterBy);
+					currentTrail.filtering.splice(index, 1);
+
+					if (!currentTrail.filtering.length) {
+						currentTrail.classList.remove("hideItem");
+						currentTrail.classList.add("showItem");
+					}
+				} else {
+					// hide trail
+					currentTrail.filtering.push(filterBy);
+					currentTrail.classList.remove("showItem");
+					currentTrail.classList.add("hideItem");
+
+				}	
+	        }
+		}
+	}
+
 }
